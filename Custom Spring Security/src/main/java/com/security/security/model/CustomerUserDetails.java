36e7eb0699439e30a06854b8b44f660ff0a42c4d
sonisg <38506968+sonisg@users.noreply.github.com>
2022-09-26 12:07:27 +0530

    Add files via upload

---
+++ Custom Spring Security/src/main/java/com/security/security/model/CustomerUserDetails.java
@@ -0,0 +1,56 @@
+package com.security.security.model;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.Collection;
+import java.util.HashSet;
+
+public class CustomerUserDetails implements UserDetails{
+
+     private User user1;
+
+    public CustomerUserDetails(User user1) {
+        this.user1 = user1;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        HashSet<SimpleGrantedAuthority> set = new HashSet<>();
+        set.add(new SimpleGrantedAuthority(this.user1.getRole()));
+        return set;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.user1.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return this.user1.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
