a7c0b2d42487e3eb4ea8394bbce9cee0335caf85
sonisg <38506968+sonisg@users.noreply.github.com>
2022-10-13 21:01:40 +0530

    Add files via upload

---
+++ Spring Security/JWT/src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
@@ -0,0 +1,58 @@
+package com.eazybytes.config;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+import javax.servlet.http.HttpServletRequest;
+
+import com.eazybytes.filter.JWTTokenGeneratorFilter;
+import com.eazybytes.filter.JWTTokenValidatorFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+@Configuration
+public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	/**
+	 * /myAccount - Secured /myBalance - Secured /myLoans - Secured /myCards -
+	 * Secured /notices - Not Secured /contact - Not Secured
+	 */
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().
+				cors().configurationSource(new CorsConfigurationSource() {
+			@Override
+			public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+				CorsConfiguration config = new CorsConfiguration();
+				config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+				config.setAllowedMethods(Collections.singletonList("*"));
+				config.setAllowCredentials(true);
+				config.setAllowedHeaders(Collections.singletonList("*"));
+				config.setExposedHeaders(Arrays.asList("Authorization"));
+				config.setMaxAge(3600L);
+				return config;
+			}
+		}).and().csrf().ignoringAntMatchers("/contact").csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()).and().
+				addFilterAfter(new JWTTokenGeneratorFilter(), BasicAuthenticationFilter.class).
+				addFilterBefore(new JWTTokenValidatorFilter(), BasicAuthenticationFilter.class).
+				authorizeRequests().antMatchers("/myAccount").authenticated().antMatchers("/myBalance").authenticated()
+				.antMatchers("/myLoans").authenticated().antMatchers("/myCards").authenticated()
+				.antMatchers("/user").authenticated().antMatchers("/notices").permitAll()
+				.antMatchers("/contact").permitAll().and().httpBasic();
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+}
